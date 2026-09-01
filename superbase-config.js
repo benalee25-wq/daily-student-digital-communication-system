1b7c6591b22d780e3243f573d742e2e9f0492b65
benalee25-wq <benalee25@gmail.com>
2026-09-02 00:49:21 +0100

    Update superbase-config.js

--- superbase-config.js
+++ superbase-config.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = "https://ilealjmhymujcoolptbe.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_eyZTQOA0_mLdxqVH3LUmvw_gsll1hBi";
 
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
